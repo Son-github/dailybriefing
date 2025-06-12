a9751ecc254cf71b4c6d52dc6882016b4dd8fe3d
son <jongin718@gmail.com>
2025-06-12 17:13:57 +0900

    feat: jwt 인증 추가 but 아직 다 못함

--- backend/src/main/java/com/sonny/backend/domain/user/service/userservice/UserService.java
+++ backend/src/main/java/com/sonny/backend/domain/user/service/userservice/UserService.java
@@ -2,12 +2,11 @@ package com.sonny.backend.domain.user.service.userservice;
 
 import com.sonny.backend.domain.user.domain.User;
 import com.sonny.backend.domain.user.dto.UserLoginRequestDto;
-import com.sonny.backend.domain.user.dto.UserResponseDto;
+import com.sonny.backend.domain.user.dto.UserLoginResponseDto;
 import com.sonny.backend.domain.user.dto.UserSignupRequestDto;
 import com.sonny.backend.domain.user.repository.UserRepository;
-import jdk.jshell.spi.ExecutionControl;
+import com.sonny.backend.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,31 +18,28 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder; // SecurityConfig 필요함.
+    private final JwtProvider jwtProvider;
 
     /* 확인해봐야할 것
      *  1. RuntimeException 모아서 쉽게 수정
      *  2. userResponseDto를 할 때, 어떤 정보들 가져와줄껀지 결정
      *  3. 보안 측면 더 올리기
      *  4. userRepository를 활용해서 save할 때, lastLoginAt만 update하면 되지 않은지?*/
-    public UserResponseDto signup(UserSignupRequestDto dto) {
+    public boolean signup(UserSignupRequestDto dto) {
         if (userRepository.findByEmail(dto.getEmail()).isPresent()) {
             throw new RuntimeException("Email Already Exists");
         }
+
         User user = User.builder()
                 .email(dto.getEmail())
                 .password(passwordEncoder.encode(dto.getPassword()))
                 .location(dto.getLocation())
                 .createdAt(ZonedDateTime.now())
                 .build();
+
         User saved = userRepository.save(user);
 
-        return UserResponseDto.builder()
-                .userId(saved.getUserId())
-                .email(saved.getEmail())
-                .location(saved.getLocation())
-                .createdAt(saved.getCreatedAt())
-                .lastLoginAt(saved.getLastLoginAt())
-                .build();
+        return true;
     }
 
     /* 확인해봐야할 것
@@ -51,7 +47,7 @@ public class UserService {
     *  2. userResponseDto를 할 때, 어떤 정보들 가져와줄껀지 결정
     *  3. 보안 측면 더 올리기
     *  4. userRepository를 활용해서 save할 때, lastLoginAt만 update하면 되지 않은지?*/
-    public UserResponseDto login(UserLoginRequestDto dto) {
+    public UserLoginResponseDto login(UserLoginRequestDto dto) {
         User user = userRepository.findByEmail(dto.getEmail())
                 .orElseThrow(() -> new RuntimeException("Email Not Found"));
 
@@ -62,16 +58,8 @@ public class UserService {
         user.setLastLoginAt(ZonedDateTime.now());
         userRepository.save(user);
 
-        return toResponseDto(user);
-    }
+        String token = jwtProvider.generateToken(user.getEmail());
 
-    private UserResponseDto toResponseDto(User user) {
-        return UserResponseDto.builder()
-                .userId(user.getUserId())
-                .email(user.getEmail())
-                .location(user.getLocation())
-                .createdAt(user.getCreatedAt())
-                .lastLoginAt(user.getLastLoginAt())
-                .build();
+        return new UserLoginResponseDto(token, user.getEmail(), user.getLocation());
     }
 }
