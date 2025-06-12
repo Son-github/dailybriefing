f42905bfe9f518f614e406bd51610de3f98fff22
son <jongin718@gmail.com>
2025-06-12 21:59:39 +0900

    feat: jwt secretkey 수정

--- backend/src/main/java/com/sonny/backend/global/jwt/JwtProvider.java
+++ backend/src/main/java/com/sonny/backend/global/jwt/JwtProvider.java
@@ -1,10 +1,10 @@
 package com.sonny.backend.global.jwt;
 
-import com.sonny.backend.domain.user.dto.UserSignupRequestDto;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -13,9 +13,16 @@ import java.util.Date;
 @Component
 public class JwtProvider {
 
-    private final String SECRET_KEY = "this_is_my_super_secret_key_for_security_2024";
-    private final long expirationMs = 1000 * 60 * 60;
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private final long expirationMs;
+    private final Key key;
+
+    public JwtProvider(
+            @Value("${jwt.secret}") String secretKey,
+            @Value("${jwt.expiration}") long expirationMs
+    ) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.expirationMs = expirationMs;
+    }
 
     public String generateToken(String email) {
         return Jwts.builder()
