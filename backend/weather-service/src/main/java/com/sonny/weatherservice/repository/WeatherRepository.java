c41e787ccc62513d0f8435576d7602f8297299ad
son <jongin718@gmail.com>
2025-06-24 00:43:05 +0900

    fix: weather-service 수정 및 docker 파일들 수정

--- backend/weather-service/src/main/java/com/sonny/weatherservice/repository/WeatherRepository.java
+++ backend/weather-service/src/main/java/com/sonny/weatherservice/repository/WeatherRepository.java
@@ -4,6 +4,10 @@ import com.sonny.weatherservice.domain.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
+public interface WeatherRepository extends JpaRepository<Weather, UUID> {
+    Optional<Weather> findTopByLocationOrderByUpdateAtDesc(String location);
 }
