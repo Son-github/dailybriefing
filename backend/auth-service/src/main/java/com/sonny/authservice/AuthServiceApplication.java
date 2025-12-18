356b086ed356f42f3c9b03eb9355954270def335
son <jongin718@gmail.com>
2025-12-18 15:30:02 +0900

    fix: add auth-service to ecs

--- backend/auth-service/src/main/java/com/sonny/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/sonny/authservice/AuthServiceApplication.java
@@ -9,9 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing
 @SpringBootApplication
 public class AuthServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
-
 }
